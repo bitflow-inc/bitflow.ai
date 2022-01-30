f1658e092b70700abc232b294b44e1124b04feaa
Sungjoon Kim <methodtweak@naver.com>
2022-01-30 16:14:39 +0900

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,4 +1,4 @@
 git pull
 pkill -9 -f "go run ./main.go"
 go install bitflow.ai
-nohup go run ./main.go &> /home/bitflow/www_logs/nohup.out
+nohup go run ./main.go > /home/bitflow/www_logs/nohup.out &
